526db3b60a0002778c84450e180b46f6eb52086a
LAPTOP-LPGD8R5F\sonof <sonofelice0@gmail.com>
2016-12-03 14:16:54 +0800

    test

--- views_test/layouts/application.php
+++ views_test/layouts/application.php
@@ -13,7 +13,6 @@
     <link rel="stylesheet" type="text/css" href="/bucket_lists/webroot/assets/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="/bucket_lists/webroot/assets/css/main.css">
     <link rel="stylesheet" type="text/css" href="/bucket_lists/webroot/assets/css/timeline.css">
-
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -55,9 +54,13 @@
         <!-- /.container-fluid -->
     </nav>
 
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 content-margin-top">
 
-    <?php include('views/'.$this->resouce.'/'.$this->action.'.php'); ?>
-
+        </div>
+      </div>
+    </div>
 
     <div class="content-wrapper">
 
