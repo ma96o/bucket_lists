76ed2b44e92fd6b4e4c81e2c9467bba0440a08d4
RYOTA TAJIMA <tajima1026@gmail.com>
2016-12-04 15:55:53 +0800

    [WIP]controller,modelのロジック部分を追加実装

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -5,6 +5,10 @@
     $controller = new UsersController($resource, $action);
 
     switch ($action) {
+      case 'pre_check';
+        $controller->pre_check();
+        break;
+
       case 'check':
         $controller->check();
         break;
@@ -87,11 +91,104 @@
         $this->viewOptions = array('name' => '','email' => '','password' => '',);
       }
 
+      function pre_check() {
+        special_echo('Controllerのpre_check()が呼び出されました。');
+        $errors = array();
+
+        if(empty($_POST)) {
+          header("Location: ");
+          exit();
+        } else{
+
+        $email = isset($_POST['email']) ? $_POST['email'] : NULL;
+
+        if ($email == ''){
+          $errors['email'] = "メールが入力されていません。";
+        } else{
+          if(!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $email)){
+            $errors['mail_check'] = "メールアドレスの形式が正しくありません。";
+          }
+          }
+        }
+
+        if (count($errors) === 0){
+
+        $url_token = hash('sha256',uniqid(rand(),1));
+        $url = "http://bucket-list.sakura.ne.jp/reg_practice/reg_form.php"."?url_token=".$url_token;
+
+        //メールの宛先
+        $emailTo = $email;
+        //Return-Pathに指定するメールアドレス
+        $returnMail = 'bucket-lists@bucket-list.sakura.ne.jp';
+        $nick_name = 'Bucket Lists.inc';
+        $subject = '[Bucket Lists]新規アカウント登録用URLのお知らせ';
+
+$body = <<< EOM
+24時間以内に下記のURLからご登録下さい。
+{$url}
+EOM;
+
+        mb_language('ja');
+        mb_internal_encoding('utf8');
+
+        //Fromヘッダーを作成
+        $header = "MIME-Version: 1.0\n";
+        $header .= "Content-Transfer-Encoding: 7bit\n";
+        $header .= "Content-Type: text/plain; charset=ISO-2022-JP\n";
+        $header .= "Message-Id: <" . md5(uniqid(microtime())) . "@gmail.com>\n";
+        $header .= 'From: bucket-lists@bucket-list.sakura.ne.jp';
+        $header .= "Reply-To: bucket-lists@bucket-list.sakura.ne.jp\n";
+        $header .= "Return-Path: bucket-lists@bucket-list.sakura.ne.jp\n";
+        $header .= "X-Mailer: PHP/". phpversion();
+
+        if(mb_send_mail($emailTo, $subject, $body, $header, '-f' . $returnMail)) {
+        //セッション変数を全て解除
+        $_SESSION = array();
+
+        //クッキーの削除
+        if (isset($_COOKIE["PHPSESSID"])) {
+          setcookie("PHPSESSID", '', time() - 1800, '/');
+        }
+        //セッションを破棄する
+        session_destroy();
+
+        $message = "メールを送信しました。24時間以内にメールに記載されたURLからご登録下さい。";
+        } else {
+          echo $errors['email_error'] = "メールの送信に失敗しました。";
+          }
+        }
+
       function check() {
         special_echo('Controllerのcheck()が呼び出されました。');
-        $this->c = $_SESSION['users'];
-        $this->action = 'check';
-        $this->display;
+        $_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
+        $token = $_SESSION['token'];
+        header('X-FRAME-OPTIONS: SAMEORIGIN');
+
+        if ($_POST['token'] != $_SESSION['token']){
+          echo "不正アクセスの可能性あり";
+        exit();
+        }
+
+        if ($nick_name == '') {
+          $errors['nick_name'] = "アカウントが入力されていません。";
+        } elseif(mb_strlen($nick_name)>15) {
+          $errors['nick_name_length'] = "アカウントは15文字以内で入力して下さい。";
+        }
+
+        //パスワード入力判定
+        if ($password == '') {
+          $errors['password'] = "パスワードが入力されていません。";
+        } elseif(!preg_match('/^[0-9a-zA-Z]{5,30}$/', $_POST["password"])) {
+          $errors['password_length'] = "パスワードは半角英数字の5文字以上30文字以下で入力して下さい。";
+        } else {
+          $password_hide = str_repeat('*', strlen($password));
+        }
+      }
+
+        if (count($errors) === 0) {
+          $_SESSION['nick_name'] = $nick_name;
+          $_SESSION['password'] = $password;
+        }
       }
 
       function create($post) {
--- models/user.php
+++ models/user.php
@@ -8,12 +8,50 @@
         $this->dbconnect = $db;
       }
 
+      function pre_check() {
+        try{
+          $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+          $statement = $dbh->prepare("INSERT INTO pre_members (url_token,email,date) VALUES (:url_token,:email,now() )");
+
+          $statement->bindValue(':url_token', $url_token, PDO::PARAM_STR);
+          $statement->bindValue(':email', $email, PDO::PARAM_STR);
+          $statement->execute();
+
+          $dbh = null;
+
+          } catch (PDOException $e){
+          echos('Error:'.$e->getMessage());
+          die();
+          }
+      }
+
       function check(){
       }
+
       function create(){
+        $sql = sprintf('INSERT INTO `members` SET
+                `nick_name`="%s",
+                `email`="%s",
+                `password`="%s",
+                `created`=NOW()',
+                mysqli_real_escape_string($db,$_SESSION['nick_name']),
+                mysqli_real_escape_string($db,$_SESSION['email']),
+                mysqli_real_escape_string($db,sha1($_SESSION['password']))
+                );
+            mysqli_query($db,$sql) or die(mysqli_error($db));
+
+        $_SESSION = array();
+
+        if (isset($_COOKIE["PHPSESSID"])) {
+              setcookie("PHPSESSID", '', time() - 1800, '/');
+        }
+        session_destroy();
       }
+
       function thanks(){
       }
+
       function login(){
       }
       function logout(){
