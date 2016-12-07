3e69df3835012dd75320a7b26ee075342d2c3f50
LAPTOP-LPGD8R5F\sonof <sonofelice0@gmail.com>
2016-12-07 11:59:59 +0800

    [WIP] create系あとはページ作って確認する

--- models/list.php
+++ models/list.php
@@ -11,11 +11,10 @@
       function create($post){$sql = sprintf('INSERT INTO `lists` SET `list_id` = %d,
 				                                                     `list_name` = "%s",
 				                                                     `user_id` = %d,
-				                                                     `create` = NOW()',
-                        mysqli_real_escape_string($this->dbconnect,$post['item_id']),
+				                                                     `created` = NOW()',
+                        mysqli_real_escape_string($this->dbconnect,$post['list_id']),
                         mysqli_real_escape_string($this->dbconnect,$post['list_name']),
-                        mysqli_real_escape_string($this->dbconnect,$post['user_id']),
-                        mysqli_real_escape_string($this->dbconnect,$post['create'])
+                        mysqli_real_escape_string($this->dbconnect,$post['user_id'])
                     );
             mysqli_query($this->dbconnect, $sql) or die(mysqli_error($this->dbconnect));
         }
