ce042b3e9f8b8b8ae64295045baae1549e389248
ErikoMatsuo <dropkick.mufufu@gmail.com>
2016-12-08 21:41:39 +0800

    show_success show_trashページ作成完了

---
+++ views/items/show_trash.php
@@ -0,0 +1,41 @@
+   <div class="modal fade" id="item_trash" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-lg">  
+          <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4></h4>
+            </div>
+          <div class="modal-body">
+              <section class="modal-box">
+                   
+                  <!--項目詳細-->
+                  <div class="item">
+                    <h4><i class="fa fa-trash-o" aria-hidden="true"></i></i>  エベレストに登っちゃうよん</h4>
+                    <p>やっぱりイモトにはなれないと思って諦めました。</p>
+
+                    <br>
+
+                  <div class="row">
+                      <div class="col-sm-4">
+                        <label><i class="fa fa-calendar" aria-hidden="true"></i></i>&nbsp;捨てた日</label>2016/7/10
+                      </div>
+
+                      <div class="col-sm-4">
+                        <label><i class="fa fa-play-circle-o" aria-hidden="true"></i>&nbsp;status</label>give up
+                      </div>
+
+                      <div class="col-sm-4">
+                        <label><i class="fa fa-star" aria-hidden="true"></i>&nbsp;ワクワク度</label>
+                        <!--0-1の画像で出し分け-->
+                        <img src="image/1.png">
+                      </div>
+
+                  </div>
+
+                   <br>
+                   <br>
+              </section>
+          </div>
+        </div>
+    </div>
+  </div>
