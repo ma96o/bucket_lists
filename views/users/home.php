6f7546a63085f95bcefdc67a9666171c83d141e3
ErikoMatsuo <dropkick.mufufu@gmail.com>
2016-12-12 10:15:18 +0800

    [WIP]homeページ作成中

---
+++ views/users/home.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bucket Lists</title>
+
+<!--     <link href="https://fonts.googleapis.com/css?family=Cabin+Sketch" rel="stylesheet"> -->
+<link href="https://fonts.googleapis.com/css?family=Rancho" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="../webroot/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../webroot/assets/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../webroot/assets/css/main.css">
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!--ワクワク度-->
+    <script type="text/javascript" src="../webroot/assets/js/jquery.raty.js"></script>
+    
+    <script src="../webroot/assets/js/bootstrap.min.js"></script>
+    <!--content style-->
+    <script src="../webroot/assets/js/pin.js"></script>
+    
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+
+<body>
+  <div class="warapper">
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#page-top">BUCKET LISTS</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <!-- <li class="page-scroll">
+                        <a href="items_trend.html">login</a>
+                    </li> -->
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+</div>
+
+
+<div class="bg">
+    <div class="container">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="content-main">
+                <div class="col-md-8 site_info">
+                    <h1>BUCKET LIST</h1>
+                    <p>死ぬまでにしたい100のことを共有して、同じ思いを持つひとと繋がろう。</p>
+                </div>
+                <div class="col-md-4">
+                    <br />
+                    <div class="side_content">
+                    <h4><i class="fa fa-pencil" aria-hidden="true"></i>&nbsp;アカウント登録</h4>
+                        <form class="form-inline">
+                            <div class="form-group">
+                                <p>メールアドレスを入力して下さい</p>
+                                <input type="email" class="form-control" placeholder="hogehoge@egmail.com">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <button class="btn btn-pink" type="submit" style="float: right;">登録</button>
+                            </div>
+                        </form>
+                    </div>
+                    <br />
+                    <div class="side_content">
+                    <h4><i class="fa fa-pencil" aria-hidden="true"></i>&nbsp;ログイン</h4>
+                    <form>
+                        <div class="form-group">
+                            <input type="email" class="form-control pd" placeholder="hogehoge@egmail.com">
+                            <input type="password" class="form-control pd" placeholder="password">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <button class="btn btn-pink" type="submit" style="float: right;">ログイン</button>
+                        </div>
+                    </form>
+                    </div>
+                </div>
+            </div>
+
+            
+          </div>
+        </div>
+    </div> 
+</div>   
+
+<!-- <div class="bg_w"> -->
+<div class="bg_middle dis">
+    <div class="container">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-10 col-xs-offset-1">    
+                    <h2>What&nbsp;&nbsp;is&nbsp;&nbsp;BUCKETLIST&nbsp;?</h2>
+                    <p>死ぬまでにしたい１００のことを書き留めるリストです。<br />
+                    <br />
+                    １つのリストに１つずつ夢を描く。<br />
+                    死ぬまでにしたいことを考える時間は、これからの未来を考えるきっかけになります。<br />
+                    <br />
+                    バケットリストを作ることで人生はもっと豊かになるはず。<br /></p>
+                    <br />
+                    <h2>BUCKETLISTでできること</h2>
+                    <p>Bucket Listsで自分のバケットリストを公開すれば、<br />
+                    友達としたいことを応援しあったり、<br />
+                    同じ思いを持った人とつながることができます。<br />
+                    考える楽しさ、成し遂げる喜びを<br />
+                    友達と共有すれば、つながりは深くなり、<br />
+                    あなたもきっと実現に近づくことができます。</p>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+<!-- </div> -->
+
+
+  
+
+ 
+ 
+<footer class="container-fluid" style="min-height:200px; background-color:#00bcd4;color:#fff;text-align:center;padding-top:50px;">
+            BUCKET LISTS 
+</footer>
+
