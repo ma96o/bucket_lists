0adda222f1ca52cb6f0bd446d646a7f4c7e87aed
ErikoMatsuo <dropkick.mufufu@gmail.com>
2016-12-14 16:20:33 +0800

    modal追加ページ

--- views/items/show.php
+++ views/items/show.php
@@ -4,7 +4,7 @@
           
           <div class="modal-header">
              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-             <h4 class="modal-title" id="item_name"><i class="fa fa-sticky-note-o" aria-hidden="true"></i>&nbsp;</h4>
+             <h4 class="modal-title" id="item_name"><i class="fa fa-trash-o" aria-hidden="true"></i><i class="fa fa-sticky-note-o" aria-hidden="true"></i>&nbsp;</h4>
           </div>
           
           <div class="modal-body">
