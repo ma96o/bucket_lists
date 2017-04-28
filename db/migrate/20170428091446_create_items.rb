daf324b59f1958b023a05b8da67e26f489cbc62a
masaaki1915 <masaaki1915@gmail.com>
2017-04-28 18:16:25 +0900

    create Item model

---
+++ db/migrate/20170428091446_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.text :comment
+      t.datetime :dead_line
+      t.integer :status
+      t.integer :priority
+      t.references :user_id
+      t.references :list_id
+
+      t.timestamps
+    end
+  end
+end
