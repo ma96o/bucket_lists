1d8f90ada7ad4f9c2eee977da609e5c1b5b2d8c2
masaaki1915 <masaaki1915@gmail.com>
2017-04-28 19:20:24 +0900

    create List model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428093326) do
+ActiveRecord::Schema.define(version: 20170428101818) do
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -26,10 +26,19 @@ ActiveRecord::Schema.define(version: 20170428093326) do
     t.index ["user_id_id"], name: "index_items_on_user_id_id", using: :btree
   end
 
+  create_table "lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "lists", "users"
 end
